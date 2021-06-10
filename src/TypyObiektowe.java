d7a22d61fb942ee6de39c7ea05f2d431a9d34c3c
Magdalena Zagwojska <magdalena.zagwojska@gmail.com>
2021-06-10 11:01:16 +0200

    HelloWorld

---
+++ src/TypyObiektowe.java
@@ -0,0 +1,8 @@
+public class TypyObiektowe {
+    String hello;
+
+    public static void main(String[] args) {
+        String hello ="Hello";
+        System.out.println(hello.charAt(0));
+    }
+}
---
+++ src/TypyProste.java
@@ -0,0 +1,10 @@
+public class TypyProste {
+    // liczby stałoprzecinkowe
+    int thirdNumber = 212954654;
+    //wartość logiczna
+    boolean prawda= true;
+    boolean fałsz=false;
+    //pojedynczy znak
+    char letter ='a';
+
+}
