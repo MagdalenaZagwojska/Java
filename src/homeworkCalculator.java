a44bc71440ea71cbcfd21d5aadb691169d76b8bf
Magdalena Zagwojska <magdalena.zagwojska@gmail.com>
2021-06-28 13:21:00 +0200

    homework

--- src/homeworkCalculator.java
+++ src/homeworkCalculator.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 public class homeworkCalculator {
     public static void main(String[] args) {
-        Scanner pingwin = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, give me a first number: ");
-        int firstNumber = pingwin.nextInt();
+        int firstNumber = scanner.nextInt();
         System.out.println("First number is: " + firstNumber);
         System.out.println("And now, give me a second number: ");
-        int secondNumber = pingwin.nextInt();
+        int secondNumber = scanner.nextInt();
         System.out.println("Second number is: " + secondNumber);
-        int resultOfAddition = firstNumber +secondNumber;
-        System.out.println("The result of adding the first number and the second number is " + resultOfAddition);
+        int resultOfAddition = firstNumber + secondNumber;
+        int resultOfSubtraction = firstNumber - secondNumber;
+        int resultOfMultiplication = firstNumber * secondNumber;
+        int resultOfDivision = firstNumber / secondNumber;
+        System.out.println("The result of the addition is " + resultOfAddition);
+        System.out.println("The result of the subtraction is " + resultOfSubtraction);
+        System.out.println("The result of the multiplication is " + resultOfMultiplication);
+        System.out.println("The result of the division is " + resultOfDivision);
 
     }
 }
