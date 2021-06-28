328ef733018ca05327be4ec22d0291ae966d068e
Magdalena Zagwojska <magdalena.zagwojska@gmail.com>
2021-06-28 09:45:55 +0200

    HelloWorld

---
+++ src/CustomScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CustomScanner {
+//    public static void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Proszę podaj swoje imię");
+//        String name = scanner.nextLine();
+//        System.out.println("Cześć " + name + "!");
+//    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę podaj pierwszą liczbę");
+        int firstNumber = scanner.nextInt();
+        System.out.println("kwadrat tej liczby to " + firstNumber*firstNumber);
+    }
+}
