91d269e314edb5e9fdcfc062d47d7569a073614f
Magdalena Zagwojska <magdalena.zagwojska@gmail.com>
2021-07-29 10:13:57 +0200

    kurs

---
+++ src/LoopCheck.java
@@ -0,0 +1,19 @@
+// wypisz liczby z zakresu 1-100 podzielne przez 3
+
+public class LoopCheck {
+    public static void main(String[] args) {
+        System.out.println("Liczby z zakresu 1-100 podzielne przez 3 to: ");
+        for (int i = 1; i<= 100; i++) {
+            if (i%3==0) {
+                System.out.println(i);
+            }
+        }
+    }
+// odwróć elementy tablicy [1,3,5,7,0] -> [0,7,5,3,1]
+
+   /* public static void main(String[] args) {
+        int[] numbers = new int[] {1,3,5,7,0};
+        System.out.println(numbers.);
+???????????
+    }*/
+}
